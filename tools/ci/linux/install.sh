de01b48e43169431f477f4a48e2b8610c45d1a12
probonopd <probono@puredarwin.org>
2015-10-11 20:00:42 +0200

    Update install.sh

--- tools/ci/linux/install.sh
+++ tools/ci/linux/install.sh
@@ -14,6 +14,8 @@ wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_essentials.7z.sha1
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z
 wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0icu_53_1_ubuntu_11_10_64.7z.sha1
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_addons.7z
+wget http://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_54/qt.54.gcc_64/5.4.1-0qt5_addons.7z.sha1
 
 7z x 5.4.1-0qt5_essentials.7z > /dev/null
 7z x 5.4.1-0icu_53_1_ubuntu_11_10_64.7z > /dev/null
